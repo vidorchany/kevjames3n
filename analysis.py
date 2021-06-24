15ccfe08817417fc0c72373426abf09d1007216f
vidorchany <liao7116660534@yeah.net>
2021-06-24 16:52:21 +0800

    First attempt at question 3C to go via the risky route to get to 10 size reward

--- analysis.py
+++ analysis.py
@@ -15,7 +15,7 @@
 
 def question2():
   answerDiscount = 0.9
-  answerNoise = 0.2
+  answerNoise = 0
   return answerDiscount, answerNoise
 
 def question3a():
@@ -33,9 +33,9 @@ def question3b():
   # If not possible, return 'NOT POSSIBLE'
 
 def question3c():
-  answerDiscount = None
-  answerNoise = None
-  answerLivingReward = None
+  answerDiscount = 0.9
+  answerNoise = 0.0
+  answerLivingReward = 0.0
   return answerDiscount, answerNoise, answerLivingReward
   # If not possible, return 'NOT POSSIBLE'
 
