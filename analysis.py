d0d352bd3a4ea0abc52e5ed8ee86dd52d615417d
vidorchany <liao7116660534@yeah.net>
2021-06-28 09:20:00 +0800

    Finished question 3a and 3b, taking the closest exit while moving along the cliff and avoiding it respectivly

--- analysis.py
+++ analysis.py
@@ -19,16 +19,16 @@ def question2():
   return answerDiscount, answerNoise
 
 def question3a():
-  answerDiscount = None
-  answerNoise = None
-  answerLivingReward = None
+  answerDiscount = 0.9
+  answerNoise = .1
+  answerLivingReward = -3
   return answerDiscount, answerNoise, answerLivingReward
   # If not possible, return 'NOT POSSIBLE'
 
 def question3b():
-  answerDiscount = None
-  answerNoise = None
-  answerLivingReward = None
+  answerDiscount = 0.1
+  answerNoise = 0.1
+  answerLivingReward = -2
   return answerDiscount, answerNoise, answerLivingReward
   # If not possible, return 'NOT POSSIBLE'
 
