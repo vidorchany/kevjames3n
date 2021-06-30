2dd773518521ce0c04b5e6d0a50f27dbab383356
vidorchany <liao7116660534@yeah.net>
2021-07-01 02:54:27 +0800

    Question 3e - avoid both the exits and the cliff

--- analysis.py
+++ analysis.py
@@ -47,9 +47,9 @@ def question3d():
   # If not possible, return 'NOT POSSIBLE'
 
 def question3e():
-  answerDiscount = None
-  answerNoise = None
-  answerLivingReward = None
+  answerDiscount = 0
+  answerNoise = 0.1
+  answerLivingReward = -2
   return answerDiscount, answerNoise, answerLivingReward
   # If not possible, return 'NOT POSSIBLE'
 
